cbc7f5ce8c9150c5d4a6bc654f37ad11cf834b86
YehLi <62835672+YehLi@users.noreply.github.com>
2022-08-08 01:39:13 +0800

    Update retinanet_dualvit_b_fpn_1x_coco.py

--- object_detection/configs/dualvit/retinanet_dualvit_b_fpn_1x_coco.py
+++ object_detection/configs/dualvit/retinanet_dualvit_b_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
     '_base_/models/retinanet_r50_fpn.py',
-    '_base_/datasets/coco_detection_b1.py',
+    '_base_/datasets/coco_detection.py',
     '_base_/schedules/schedule_1x.py',
     '_base_/default_runtime.py'
 ]
@@ -21,5 +21,6 @@ model = dict(
 optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 
+runner = dict(type='EpochBasedRunner', max_epochs=12)
 fp16 = dict(loss_scale=512.)
 find_unused_parameters = True
