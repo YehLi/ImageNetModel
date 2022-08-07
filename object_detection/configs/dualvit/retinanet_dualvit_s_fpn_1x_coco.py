4b5eb7c9496dcd20692a4c6aa7984c2e2a3317b8
YehLi <62835672+YehLi@users.noreply.github.com>
2022-08-08 01:38:55 +0800

    Update retinanet_dualvit_s_fpn_1x_coco.py

--- object_detection/configs/dualvit/retinanet_dualvit_s_fpn_1x_coco.py
+++ object_detection/configs/dualvit/retinanet_dualvit_s_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
     '_base_/models/retinanet_r50_fpn.py',
-    '_base_/datasets/coco_detection_s.py',
+    '_base_/datasets/coco_detection.py',
     '_base_/schedules/schedule_1x.py',
     '_base_/default_runtime.py'
 ]
